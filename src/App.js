c0266476b05bb4fe648fb2650ece5f3bd77c6688
Himanshu-varni <himanshu.varni@gmail.com>
2023-09-06 18:58:04 +0530

    adeed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/products/?populate=deep`).then(({ data }) => {
+      setData(data.data);
+    });
+  }, []);
+
+  console.log("data", data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    ok
+    {data.map((value) => (
+      <h1>{value.attributes.title}</h1>
+    ))}
     </div>
   );
 }
